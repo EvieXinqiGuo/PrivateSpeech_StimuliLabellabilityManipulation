bb1742402fda9330bc27aadaa03f075a480b0137
xinqi <guoxinqieve@gmail.com>
2022-06-12 17:16:55 -0700

    create empty columns of trial 5-12
    not used eventually due to the manual way being easier. but the intention was to create empty columns of trial 5-12 for categorization

--- insertEmptyColumnsForCategorization.R
+++ insertEmptyColumnsForCategorization.R
@@ -5,18 +5,20 @@
 
 setwd("/Users/guoxinqieve/Applications/OneDrive - UC San Diego/Spring2022_nameability_pilot transcription/types/categorizing process with raw data")
 Participants = list.files("/Users/guoxinqieve/Applications/OneDrive - UC San Diego/Spring2022_nameability_pilot transcription/types/categorizing process with raw data", ".csv")
-
-tmp <- read.csv("original_file.csv")
-tmp <- cbind(tmp, new_column)
-write.csv(tmp, "modified_file.csv")
-
+ 
 O = lapply(Participants, function(x) {
-  DF = read.csv(x, header = T, na.strings=c("","NA"), sep = ",")
-  DF1 = DF %>%
-    mutate(wordCountTrial3 = sum(str_count(Trial3, "\\w+"), na.rm = T),
-           wordCountTrial4 = sum(str_count(Trial4, "\\w+"), na.rm = T),
-           utteranceCountTrial3 = nrow(na.omit(as.data.frame(Trial3))),
-           utteranceCountTrial4 = nrow(na.omit(as.data.frame(Trial4)))) %>%
-    select(wordCountTrial3, wordCountTrial4, utteranceCountTrial3, utteranceCountTrial4) %>%
-    distinct() 
-  return(DF1)})
+  DF = read.csv(x, header = T, na.strings = c("","NA"), sep = ",")
+  newstuff_df <- data.frame(Categorized5 = double(), 
+                            Categorized6 = double(), 
+                            Categorized7 = double(),
+                            Categorized8 = double(), 
+                            Categorized9 = double(), 
+                            Categorized10 = double(), 
+                            Categorized11 = double(), 
+                            Categorized12 = double())
+  DF1 = rbind(DF, new_column)
+  return(DF1)
+  write.csv(x, file = paste0("categorizing_", x, row.names = FALSE))
+})
+
+O
